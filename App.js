0e776e62f92c4b4335acfa6f50cf21d2729b22ef
Allison Sobol <allisonsobol13@gmail.com>
2023-09-21 16:01:55 -0700

    Imported Chart.js and working on getting a graph

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import ChartComponent from './src/components/Graph.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <div className="App">
+        <h1>Chart.js in React!</h1>
+        <ChartComponent />
+      </div>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Graph.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Bar } from 'react-chartjs-2';
+
+
+const ChartComponent = () => {
+
+  return (
+    <div>
+      <h4>This is going to be where the chart goes!</h4>
+    </div>
+  );
+};
+
+export default ChartComponent;
