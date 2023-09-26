e14831f658aad3a0ec387df0e8045993ccf2ae11
Allison Sobol <allisonsobol13@gmail.com>
2023-09-25 17:32:52 -0700

    Styled the headings for the page

--- App.js
+++ App.js
@@ -6,55 +6,54 @@ import ChartComponent from './src/components/Graph.js';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <View style={styles.leftText}>
-          <Text><h3>PA-44-180, SEMINOLE</h3></Text>
-        </View>
-        <View style={styles.rightText}>
-          <Text><h3>SECTION 5</h3></Text>
-          <Text><h3>PERFORMANCE</h3></Text>
-        </View>
+    <View style={styles.topRow}>
+      <Text style={styles.leftText}>PA-44-180, SEMINOLE</Text>
+      <View style={styles.rightContainer}>
+        <Text style={styles.rightText}>SECTION 5</Text>
+        <Text style={styles.rightBottomText}>PERFORMANCE</Text>
       </View>
-      <View style={styles.line}></View> 
-      <View style={styles.bottomContainer}>
-        <Text><h4>TAKEOFF DISTANCE OVER 50 FT OBSTACLE - SHORT FIELD EFFORT</h4></Text>
-        <Text>The thingy here</Text>
-        <ChartComponent />
-      </View>
-      <StatusBar style="auto" />
     </View>
-  );
-}
+    <Text style={styles.title}>TAKEOFF DISTANCE OVER 50 FT OBSTACLE - SHORT FIELD EFFORT</Text>
+    <ChartComponent />
+    <StatusBar style="auto" />
+  </View>
+);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  topContainer: {
-    flex: 2,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  bottomContainer: {
-    flex: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  leftText: {
-    flex: -1,
-    alignItems: 'flex-start',
-  },
-  rightText: {
-    flex: -1,
-    alignItems: 'flex-end',
-  },
-  line: {
-    borderBottomWidth: 25,
-    borderBottomColor: 'black',
-    marginVertical: 10,
-  },
+container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  paddingTop: 50,
+},
+topRow: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  width: '100%',
+  paddingHorizontal: 20,
+},
+leftText: {
+  fontSize: 16,
+  fontWeight: 'bold',
+},
+rightContainer: {
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+},
+rightText: {
+  fontSize: 16,
+  fontWeight: 'bold',
+},
+rightBottomText: {
+  fontSize: 16,
+  fontWeight: 'bold',
+},
+title: {
+  fontSize: 24,
+  fontWeight: 'bold',
+  marginTop: 20,
+},
 });
 
 
