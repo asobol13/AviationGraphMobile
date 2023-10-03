a639c1320526716bc7e8a4a8465bc769720f9050
Allison Sobol <allisonsobol13@gmail.com>
2023-10-02 17:34:57 -0700

    Added middle content, working on divider line

--- App.js
+++ App.js
@@ -16,28 +16,48 @@ export default function App() {
     <Text style={styles.title}>TAKEOFF DISTANCE OVER 50 FT OBSTACLE - SHORT FIELD EFFORT</Text>
     <View style={styles.conditionsContainer}>
     <View style={styles.conditionsLC}> {/*beginning of boxes */}
-      <View style={styles.conditionsLT}>
-        <Text style={styles.conditionsTxt}>
-          ASSOCIATED CONDITIONS:{'\n'}    Wing Flaps:  0{'\n'}    Cowl Flaps:  OPEN
-          {'\n'}    Power:          2700 RPM & FULL THROTTLE
-          {'\n'}                         BEFORE BRAKE RELEASE
-          {'\n'}    Runway:        PAVED, LEVEL & DRY
-        </Text>
-      </View>
+      <Text style={styles.conditionsTxt}>
+        ASSOCIATED CONDITIONS:{'\n'}    Wing Flaps:  0{'\n'}    Cowl Flaps:  OPEN
+        {'\n'}    Power:          2700 RPM & FULL THROTTLE
+        {'\n'}                         BEFORE BRAKE RELEASE
+        {'\n'}    Runway:        PAVED, LEVEL & DRY
+      </Text>
     </View>
     <View style={styles.conditionsMC}>
-      <Text style={styles.conditionsTxt}>This is where the boxes will go</Text>
+      <View style={styles.conditionsMLB}>
+        <View style={styles.section1}>
+          <Text style={styles.sectionHeading}>WEIGHT{'\n'}POUNDS</Text>
+          <View style={styles.sectionWithBorder}></View>
+          <Text style={styles.sectionTxt}>3800{'\n'}3400{'\n'}3000{'\n'}2600</Text>
+        </View>
+        <View style={styles.section2}>
+          <Text style={styles.sectionHeading}>ROTATE{'\n'}SPEED KIAS</Text>
+          <View style={styles.sectionWithBorder}></View>
+          <Text style={styles.sectionTxt}>70{'\n'}66{'\n'}62{'\n'}57</Text>
+        </View>
+        <View style={styles.section3}>
+          <Text style={styles.sectionHeading}>OBSTACLE{'\n'}SPEED KIAS</Text>
+          <View style={styles.sectionWithBorder}></View>
+          <Text style={styles.sectionTxt}>82{'\n'}77{'\n'}72{'\n'}67</Text>
+        </View>
+      </View>
+      <View style={styles.conditionsMRB}>
+        <View style={styles.cautionBoxTxt}>
+          <Text style={styles.cautionTxt}>CAUTION</Text>
+          <Text style={styles.cautionSubTxt}>BEST ONE ENGINE OPERATIVE RATE OF CLIMB IS LESS THAN 50 FPM IF T.O.WT. IS IN THE SHADED
+            AREA.
+          </Text>
+        </View>
+      </View>
     </View>
     <View style={styles.conditionsRC}>
-    <View style={styles.conditionsRT}>
-        <Text style={styles.conditionsTxt}>
-          EXAMPLE:{'\n'}    O.A.T.:                               8C
-          {'\n'}    Airport Pressure Altitude: 1250 FT
-          {'\n'}    Weight:                             3430 LBS
-          {'\n'}    Wind Component:            6 KT HEADWIND{'\n'}    Takeoff 
-          Distance{'\n'}      Over 50 FT Obstacle:      1520 FT
-        </Text>
-      </View>
+      <Text style={styles.conditionsTxt}>
+        EXAMPLE:{'\n'}    O.A.T.:                               8C
+        {'\n'}    Airport Pressure Altitude: 1250 FT
+        {'\n'}    Weight:                             3430 LBS
+        {'\n'}    Wind Component:            6 KT HEADWIND{'\n'}    Takeoff 
+        Distance{'\n'}      Over 50 FT Obstacle:      1520 FT
+      </Text>
     </View>
     </View>
     <View style={styles.graphContainer}>
@@ -96,19 +116,81 @@ conditionsLC: {
   flexDirection: 'column',
   alignItems: 'flex-start',
 },
-conditionsLT: {
-
-},
 conditionsMC: {
+  flexDirection: 'row',
+},
+conditionsMLB: {
+  flexDirection: 'row',
+  width: 300,
+  height: 100,
+  borderColor: 'black',
+  borderWidth: 1,
+  alignItems: 'stretch',
+  marginRight: 10,
+},
+section1: {
+  flex: 3,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRightColor: 'black',
+  borderRightWidth: 1,
+},
+section2: {
+  flex: 3,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRightColor: 'black',
+  borderRightWidth: 1,
+},
+section3: {
+  flex: 3,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRightColor: 'black',
+  borderRightWidth: 1,
+},
+sectionHeading: {
+  fontSize: 12,
+  fontWeight: 'bold',
+  textAlign: 'center',
+},
+sectionWithBorder: {
+  borderTopColor: 'black',
+  borderTopWidth: 1,
+  marginTop: 5,
+},
+sectionTxt: {
+  fontSize: 10,
+  marginTop: 5,
+  textAlign: 'center',
+},
+conditionsMRB: {
+  flexDirection: 'row',
+  width: 200,
+  height: 100,
+  backgroundColor: 'transparent',
+  borderColor: 'black',
+  borderWidth: 5,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+cautionBoxTxt: {
   flexDirection: 'column',
-  alignItems: 'flex-middle',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 180,
+},
+cautionTxt: {
+  fontSize: 12,
+  fontWeight: 'bold',
+  marginBottom: 5,
+},
+cautionSubTxt: {
+  fontSize: 10,
 },
 conditionsRC: {
   flexDirection: 'column',
   alignItems: 'flex-end',
-},
-conditionsRT: {
-
 },
 conditionsTxt: {
   fontSize: 12,
